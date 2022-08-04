d9981ce92bf3e8c8b737e72fe1ab660f76480819
Pepe Guerrero <pepe.guerrero@xapo.com>
2022-08-04 18:17:13 +0200

    Simulate conditions

--- app/src/main/java/com/marcorighini/lint_mre/MainActivity.kt
+++ app/src/main/java/com/marcorighini/lint_mre/MainActivity.kt
@@ -2,10 +2,13 @@ package com.marcorighini.lint_mre
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView).text =
+            getString(com.example.core.R.string.cross_module_string)
     }
 }
